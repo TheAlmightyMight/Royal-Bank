7a6ab442bf314e2b30043912c687cb309a501ae8
TheAlmightyMight <klimentykk@gmail.com>
2022-06-24 19:23:56 +0400

    Update home.jsx

--- bank/src/pages/home/home.jsx
+++ bank/src/pages/home/home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+  return (
+    <div>
+      <h1>home</h1>
+      <button onClick={navigate("/registration/step1")}>registration</button>
+    </div>
+  );
 }
 
 export default Home;
